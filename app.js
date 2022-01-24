e44fc0831f042235098ae2581b1fe17f8ce3f08d
Jan Peter <jan.peter92@mail.de>
2022-01-24 12:38:47 +0100

    feature: created files

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+// import { promises as fs } from "fs";
+import { writeFile } from "fs/promises";
+
 console.log("Hello World");
+writeFile("foo", "asdasda");
+
+writeFile("hello.json", JSON.stringify({ x: 5, y: 6 }, null, 4));
