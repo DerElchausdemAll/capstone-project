2c14024d1b78933807dbc2a1491f454c9fff997a
Jan Peter <jan.peter92@mail.de>
2022-01-24 15:39:54 +0100

    feature: readFile

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ import { writeFile } from "fs/promises";
 console.log("Hello World");
 writeFile("foo", "asdasda");
 
-writeFile("hello.json", JSON.stringify({ x: 5, y: 6 }, null, 4));
+writeFile("hello.json", JSON.stringify({ x: 5, y: "chaaaange" }, null, 4));
