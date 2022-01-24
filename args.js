9a04766ca0a9149acb744fd11c80c9449384d64e
Jan Peter <jan.peter92@mail.de>
2022-01-24 17:18:26 +0100

    process-argv-property

--- args.js
+++ args.js
@@ -2,3 +2,20 @@
 // ["foo", "bar", "baz"]
 
 //command: node args.js foo bar baz
+
+const args = process.argv.slice(2);
+
+console.log(args);
+
+// import { argv } from "process";
+
+// print process.argv
+// argv.forEach((val, index) => {
+//   console.log(`${index}: ${val}`);
+// });
+
+// argv.forEach((val, index) => {
+//     if (index > 1) {
+//       console.log(`${index}: ${val}`);
+//     }
+//   });
